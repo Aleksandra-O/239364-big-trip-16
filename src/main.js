913d81ed3d7d37c95a93e052b139d6e78fd9bb84
Maxim Volkov <mx_ruz_net@hotmail.com>
2021-12-12 20:43:34 +0300

    corrects the place for nav and filter form

--- src/main.js
+++ src/main.js
@@ -8,11 +8,8 @@ import {renderTemplate, RenderPosition} from './render.js';
 
 const POINT_COUNT = 3;
 
-const siteHeaderElement = document.querySelector('.trip-main');
-const siteHeaderControlElement = siteHeaderElement.querySelector('.trip-controls');
-
-renderTemplate(siteHeaderControlElement, createSiteMenuTemplate(), RenderPosition.AFTERBEGIN);
-renderTemplate(siteHeaderControlElement, createSiteFilterTemplate(), RenderPosition.BEFOREEND);
+renderTemplate(document.querySelector('.trip-controls__navigation'), createSiteMenuTemplate(), RenderPosition.BEFOREEND);
+renderTemplate(document.querySelector('.trip-controls__filters'), createSiteFilterTemplate(), RenderPosition.BEFOREEND);
 
 const siteMainElement = document.querySelector('.trip-events');
 const siteListElement = siteMainElement.querySelector('.trip-events__list');
