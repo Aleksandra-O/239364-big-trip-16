13caf47dafa54c53aa060085dcb4fc9e77548a5b
Maxim Volkov <mx_ruz_net@hotmail.com>
2021-12-12 20:52:08 +0300

    introduces the list frame

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import {createEditPointTemplate} from './view/edit-point-view.js';
 import {createPointTemplate} from './view/point-view.js';
 
 import {renderTemplate, RenderPosition} from './render.js';
+import { createListFrameTemplate } from './view/list-frame.js';
 
 const POINT_COUNT = 3;
 
@@ -12,11 +13,13 @@ renderTemplate(document.querySelector('.trip-controls__navigation'), createSiteM
 renderTemplate(document.querySelector('.trip-controls__filters'), createSiteFilterTemplate(), RenderPosition.BEFOREEND);
 
 const siteMainElement = document.querySelector('.trip-events');
+
+renderTemplate(siteMainElement, createSiteSortTemplate(), RenderPosition.BEFOREEND);
+renderTemplate(siteMainElement, createListFrameTemplate(), RenderPosition.BEFOREEND);
+
 const siteListElement = siteMainElement.querySelector('.trip-events__list');
 
-renderTemplate(siteMainElement, createSiteSortTemplate(), RenderPosition.BEFOREBEGIN);
-
-renderTemplate(siteListElement, createEditPointTemplate(), RenderPosition.AFTERBEGIN);
+renderTemplate(siteListElement, createEditPointTemplate(), RenderPosition.BEFOREEND);
 
 for (let i = 0; i < POINT_COUNT; i++) {
   renderTemplate(siteListElement, createPointTemplate(), RenderPosition.BEFOREEND);
